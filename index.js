811e9d917c9e5feb19aa10e13a86a2b239275da0
“Foxshane997” <Foxshane997@gmail.com>
2024-03-11 20:58:50 -0700

    adding all the files

---
+++ index.js
@@ -0,0 +1,8 @@
+let playerId = [1, 2, 3, 4, 5, 6,]
+// not a string so no quotes.
+
+console.log("You are Player " + playerId[0] + ".") 
+//  this one isnt used anymore.
+
+console.log(`You are Player ${playerId[0]}.`)
+// this is the primary used version.
