b0a6527f8cdecbd12fafe319569e58e30da8d3ec
“Foxshane997” <Foxshane997@gmail.com>
2024-03-13 20:45:15 -0700

    update

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-let playerId = [1, 2, 3, 4, 5, 6,]
-// not a string so no quotes.
+//  Day One --
 
-console.log("You are Player " + playerId[0] + ".") 
-//  this one isnt used anymore.
+// let playerId = [1, 2, 3, 4, 5, 6,]
+// let x = 0 
+// // // // not a string so no quotes.
 
-console.log(`You are Player ${playerId[0]}.`)
-// this is the primary used version.
+// for (let x=0; x<playerId.length; x++){
+//     console.log('you are ${playerId[x]}.')
+// }
 
-playerId.push(`2`)
-console.log(playerId)
+// console.log("You are Player " + playerId[0] + ".") 
+// //  this one isnt used anymore.
 
-consol.log(num + string)
-console.log(string + num)
+// console.log(`You are Player ${playerId[0]}.`)
+// // this is the primary used version.
 
-// after class 
+// playerId.push(`2`)
+// console.log(playerId)
+
+// consol.log(num + string)
+// console.log(string + num)
+
+// Day Two  --
+
+// let a = '0'
+// let b = '0'
+// console.log(a == b)
+
+// if (a){
+//     console.log('A is true!')
+// }
 
